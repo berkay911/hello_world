530d4cda15092bbafbe89446bbdb7b35fec74c9e
Berkay <berkay9114@proton.me>
2026-01-05 16:39:07 +0300

    #4,#5,#6

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,24 @@ void main() {
   runApp(const MyApp());
 }
 
+class Post {
+  String body;
+  String author;
+  int likes;
+  bool userLiked = false;
+
+  Post(this.body, this.author, {this.likes = 0});
+
+  void likePost() {
+    this.userLiked = !this.userLiked;
+    if (this.userLiked) {
+      this.likes += 1;
+    } else {
+      this.likes -= 1;
+    }
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -24,18 +42,55 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  List<Post> posts = [];
+
+  void newPost(String text) {
+    this.setState(() {
+      posts.add(new Post(text, "Berkay"));
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Hello World")),
-      body: TextInputWidget(),
+      body: Column(
+        children: <Widget>[
+          Expanded(child: PostList(this.posts)),
+          TextInputWidget(this.newPost),
+        ],
+      ),
     );
   }
 }
 
 class TextInputWidget extends StatefulWidget {
-  const TextInputWidget({super.key});
+  final Function(String) callback;
+
+  TextInputWidget(this.callback);
 
   @override
   State<TextInputWidget> createState() => _TextInputWidgetState();
@@ -44,39 +99,87 @@ class TextInputWidget extends StatefulWidget {
 class _TextInputWidgetState extends State<TextInputWidget> {
   final controller = TextEditingController();
 
-  String text = "";
-
   @override
   void dispose() {
     super.dispose();
     controller.dispose();
   }
 
-  void changeText(text) {
-    if (text == "Hello World") {
-      controller.clear();
-      text = "";
-    }
+  void click() {
+    widget.callback(controller.text);
+    FocusScope.of(context).unfocus();
+    controller.clear();
+  }
 
-    setState(() {
-      this.text = text;
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: this.controller,
+      decoration: InputDecoration(
+        prefixIcon: Icon(Icons.message),
+        labelText: "Enter your message",
+        suffixIcon: IconButton(
+          icon: Icon(Icons.send),
+          splashColor: const Color.fromARGB(255, 43, 236, 4),
+          tooltip: "Post message",
+          onPressed: this.click,
+        ),
+      ),
+    );
+  }
+}
+
+class PostList extends StatefulWidget {
+  final List<Post> listItems;
+
+  PostList(this.listItems);
+
+  @override
+  State<PostList> createState() => _PostListState();
+}
+
+class _PostListState extends State<PostList> {
+  void like(Function callback) {
+    this.setState(() {
+      callback();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        TextField(
-          controller: this.controller,
-          decoration: InputDecoration(
-            prefixIcon: Icon(Icons.message),
-            labelText: "Enter your message",
+    return ListView.builder(
+      itemCount: this.widget.listItems.length,
+      itemBuilder: (context, index) {
+        var post = this.widget.listItems[index];
+        return Card(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: ListTile(
+                  title: Text(post.body),
+                  subtitle: Text(post.author),
+                ),
+              ),
+              Row(
+                children: <Widget>[
+                  Container(
+                    child: Text(
+                      post.likes.toString(),
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.thumb_up),
+                    onPressed: () => this.like(post.likePost),
+                    color: post.userLiked ? Colors.green : Colors.grey,
+                  ),
+                ],
+              ),
+            ],
           ),
-          onChanged: (text) => this.changeText(text),
-        ),
-        Text(this.text),
-      ],
+        );
+      },
     );
   }
 }
